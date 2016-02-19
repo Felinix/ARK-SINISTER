85631260738e2dd5fa7c134694876b8ee96c3077
Felinix <felinix18@hotmail.com>
2016-02-19 16:44:46 -0500

    Added Title Menu scene
    Title menu screen with buttons and events script.

---
+++ ARK SINISTER/Assets/Scripts/UI/Main Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MainMenu : MonoBehaviour {
+
+    public static void StartGame_Click()
+    {
+        Application.LoadLevel("StartGame");
+    }
+
+    public static void ContinueGame_Click()
+    {
+        // load game state from "previously played" database table.
+    }
+
+    public static void Options_Click()
+    {
+        // show options menu
+    }
+
+    public static void About_Click()
+    {
+        // show about menu
+    }
+
+    public static void Quit_Click()
+    {
+        // Exit game.
+        Application.Quit();
+    }
+}
